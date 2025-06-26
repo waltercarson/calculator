31c7e36e74414fd2561e2510e67625b73158ffd6
Walter Apps <waltercarsonapps@icloud.com>
2025-06-26 12:37:40 -0400

    added '.' button, back btn, parenthesis btns.

--- app.js
+++ app.js
@@ -16,3 +16,13 @@ function calculate() {
         display.value = "Error";
     }
 }
+
+function backspace() {
+    let currentValue = display.value; // Or displayElement.textContent if it's a div/span
+  // Remove the last character using slice(0, -1)
+    let newValue = currentValue.slice(0, -1);
+  // Update the display with the new value
+    display.value = newValue;
+}
+
+
